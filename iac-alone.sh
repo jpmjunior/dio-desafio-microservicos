be0032bfdb0b28fcd22f7298fcc81d9a4ecb78c2
jpmjunior <junior.linhares@yahoo.com.br>
2023-02-22 22:18:32 -0300

    Correção

--- iac-alone.sh
+++ iac-alone.sh
@@ -45,29 +45,3 @@ docker exec -i mysql-A sh -c 'exec mysql -u root -p"$MYSQL_ROOT_PASSWORD" meuban
 
 echo -e "###\nCriando container do Apache com PHP 7\n###"
 sudo docker run --name web-server -dt -p 80:80 --mount type=volume,src=app,dst=/app/ webdevops/php-apache:alpine-php7
-
-##########
-
-echo -e "###\nEncerrando container web-server antes de iniciar o Docker Swarm\n###"
-docker rm --force web-server
-
-echo -e "###\nIniciando cluster Docker Swarm\n###"
-docker swarm init
-
-# Executar comando para incluir workers conforme sugestão após o docker swarm init. Exemplo:
-# docker swarm join --token SWMTKN-1-1ztczbygl737vqyzimyiz5gpafr8r3ikom2f4k9eu43mm3ujwc-3h1topajspfcvekcsket129g2 192.168.0.124:2377
-#
-# Comando para verificar integrantes do cluster docker swarm:
-# docker node ls
-
-echo -e "###\nCriando 10 réplicas do conteiner Apache com PHP 7\n###"
-docker service create --name web-server --replicas 10 -dt -p 80:80 --mount type=volume,src=app,dst=/app/ webdevops/php-apache:alpine-php7
-
-echo -e "###\nIniciando e configurando NFS Server\n###"
-apt-get install nfs-server -y
-echo "/var/lib/docker/volumes/app/_data *(rw,sync,subtree_check)" >> /etc/exports
-exportfs -ar
-
-echo -e "###\nCriando container do proxy NGIX\n###"
-docker build -t proxy-app .
-docker run --name my-proxy-app -dti -p 4500:4500 proxy-app
